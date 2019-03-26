9a2de3ecda0042950eec9510b5a7dfa6f25d3442
irfaklahumu <48472902+irfaklahumu@users.noreply.github.com>
2019-03-26 16:06:00 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php
+    $kon = mysqli_connect("localhost","id8951431_penjualan","sasuke96","id8951431_irfak");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
