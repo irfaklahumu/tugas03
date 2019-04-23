f8014d478c78b08d9be914ac3155256cec38bb5a
irfaklahumu <48472902+irfaklahumu@users.noreply.github.com>
2019-04-23 15:44:04 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php
+    $kon = mysqli_connect("localhost","id8951431_topan","12345678","id8951431_topan");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
